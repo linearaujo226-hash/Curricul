314633c3602a9d21687e7771bcca0d4692de1458
LyneIA <linearaujo226@gmail.com>
2026-06-12 01:11:49 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,56 @@
+function salvarDados(){
 
+const campos = [
+"nome",
+"email",
+"telefone",
+"cidade",
+"cargo",
+"linkedin",
+"formacao",
+"experiencia",
+"habilidades",
+"conquistas"
+];
+
+campos.forEach(id => {
+
+let elemento = document.getElementById(id);
+
+if(elemento){
+localStorage.setItem(id, elemento.value);
+}
+
+});
+
+}
+
+function carregarDados(){
+
+const campos = [
+"nome",
+"email",
+"telefone",
+"cidade",
+"cargo",
+"linkedin",
+"formacao",
+"experiencia",
+"habilidades",
+"conquistas"
+];
+
+campos.forEach(id => {
+
+let elemento = document.getElementById(id);
+
+if(elemento){
+elemento.value =
+localStorage.getItem(id) || "";
+}
+
+});
+
+}
+
+window.onload = carregarDados;
