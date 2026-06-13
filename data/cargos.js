8b4ff20f34a43f6ccb638883dccd31c37ec4dfa9
LyneIA <linearaujo226@gmail.com>
2026-06-12 23:57:08 -0300

    Create cargos.js

---
+++ data/cargos.js
@@ -0,0 +1,136 @@
+const cargos = {
+
+tecnologia: [
+"Desenvolvedor Python",
+"Desenvolvedor Java",
+"Desenvolvedor Full Stack",
+"Desenvolvedor Front-End",
+"Desenvolvedor Back-End",
+"Analista de Sistemas",
+"Engenheiro de Software",
+"QA",
+"DevOps",
+"Arquiteto de Software"
+],
+
+dados: [
+"Analista de Dados",
+"Cientista de Dados",
+"Engenheiro de Dados",
+"BI Analyst",
+"Analista de BI",
+"Especialista Power BI"
+],
+
+marketing: [
+"Analista de Marketing",
+"Social Media",
+"Gestor de Tráfego",
+"Copywriter",
+"SEO",
+"Designer de Marketing"
+],
+
+financeiro: [
+"Analista Financeiro",
+"Controller",
+"Tesoureiro",
+"Assistente Financeiro",
+"Coordenador Financeiro"
+],
+
+rh: [
+"Assistente de RH",
+"Analista de RH",
+"Business Partner",
+"Recrutador",
+"Tech Recruiter"
+],
+
+comercial: [
+"Vendedor",
+"Consultor Comercial",
+"Executivo de Contas",
+"SDR",
+"Closer"
+],
+
+juridico: [
+"Advogado",
+"Analista Jurídico",
+"Assistente Jurídico"
+],
+
+administrativo: [
+"Assistente Administrativo",
+"Analista Administrativo",
+"Secretária"
+],
+
+logistica: [
+"Analista de Logística",
+"Supervisor Logístico",
+"Comprador"
+],
+
+engenharia: [
+"Engenheiro Civil",
+"Engenheiro Mecânico",
+"Engenheiro Elétrico"
+],
+
+saude: [
+"Enfermeiro",
+"Técnico de Enfermagem",
+"Fisioterapeuta",
+"Farmacêutico"
+],
+
+educacao: [
+"Professor",
+"Coordenador Pedagógico",
+"Tutor"
+],
+
+atendimento: [
+"Atendente",
+"Suporte ao Cliente",
+"Customer Success"
+],
+
+seguranca: [
+"Vigilante",
+"Técnico de Segurança do Trabalho"
+],
+
+industrial: [
+"Operador de Produção",
+"Supervisor Industrial"
+],
+
+turismo: [
+"Agente de Viagens",
+"Guia de Turismo"
+],
+
+agronegocio: [
+"Agrônomo",
+"Técnico Agrícola"
+],
+
+construcao: [
+"Mestre de Obras",
+"Orçamentista"
+],
+
+compras: [
+"Comprador",
+"Analista de Compras"
+],
+
+qualidade: [
+"Analista de Qualidade",
+"Inspetor de Qualidade"
+]
+
+};
