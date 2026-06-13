62d24322813e7b9958c07169db9477f30760a33f
LyneIA <linearaujo226@gmail.com>
2026-06-12 22:37:16 -0300

    Create vagas.js

---
+++ page/vagas.js
@@ -0,0 +1,33 @@
+const vagas = {
+
+python: [
+"Explique orientação a objetos",
+"Como funciona uma API REST?",
+"O que são decorators?"
+],
+
+dados: [
+"Explique JOIN",
+"Explique ETL",
+"Como otimizar SQL?"
+],
+
+marketing: [
+"Como medir ROI?",
+"Explique funil de vendas",
+"O que é CAC?"
+],
+
+financeiro: [
+"Explique fluxo de caixa",
+"Como calcular margem líquida?",
+"Como analisar DRE?"
+],
+
+rh: [
+"Como conduzir entrevistas?",
+"Como reduzir turnover?",
+"Como avaliar desempenho?"
+]
+
+};
