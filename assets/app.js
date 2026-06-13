d9d8866db959b92f494176b1141ee73e91421adc
LyneIA <linearaujo226@gmail.com>
2026-06-13 00:17:48 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -54,3 +54,40 @@ localStorage.getItem(id) || "";
 }
 
 window.onload = carregarDados;
+
+/* =========================
+   DASHBOARD
+========================= */
+
+function obterDashboard(){
+
+const historico =
+JSON.parse(
+localStorage.getItem("historico") || "[]"
+);
+
+return {
+
+entrevistas:
+historico.filter(
+h => h.tipo === "entrevista"
+).length,
+
+curriculos:
+historico.filter(
+h => h.tipo === "curriculo"
+).length,
+
+analises:
+historico.filter(
+h => h.tipo === "analise"
+).length,
+
+cartas:
+historico.filter(
+h => h.tipo === "carta"
+).length
+
+};
+
+}
